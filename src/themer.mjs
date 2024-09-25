f44f9fbbc245b7c8009b60fc8c90fe702e33723f
Christopher Wright <chrisw@campaignmonitor.com>
2024-09-25 12:39:34 +1000

    Update themer.mjs

--- src/themer.mjs
+++ src/themer.mjs
@@ -24,7 +24,7 @@ const program = new Command();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const rootDir = __dirname;
+const rootDir = process.cwd();
 
 
 program
